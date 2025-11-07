0a4f982f05028a108e8dff6b5e3bfe7972a2ae17
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-11-07 11:11:13 -0500

    Update ContentView.swift

--- ChipsSaveJSON/ChipsSaveJSON/ContentView.swift
+++ ChipsSaveJSON/ChipsSaveJSON/ContentView.swift
@@ -30,80 +30,8 @@ struct ContentView: View {
               }
             }
           }
-          if let item = document.selectedItem {
-            Text("x \(item.x) y \(item.y) color \(item.colorName)")
-            HStack {
-              ColorPicker("Color", selection: $document.itemColor)
-              Button("Rotate") {
-                document.update(id: document.selectedId, rotationBy: 45.0)
-              }
-              Button("+Size") {
-                document.update(id: document.selectedId, sizeBy: 1.1)
-              }
-              Button("-Size") {
-                document.update(id: document.selectedId, sizeBy: 0.9)
-              }
-            }
-            .buttonStyle(.bordered)
-            HStack {
-              Text("AssetName:")
-              Picker("AssetName", selection: $document.itemAssetName) {
-                Text("").tag("")
-                Text("cat").tag("cat")
-                Text("lama").tag("lama")
-              }
-            }
-            .padding(5)
-            .background(Color.gray)
-          }
-          HStack {
-            Button("Add") {
-              withAnimation {
-                document.addItem(rect: rect)
-              }
-            }
-            Button("+8") {
-              withAnimation {
-                //document.clear();
-                document.addItems(rect: rect, count: 8)
-              }
-            }
-            Button("Clear") {
-              withAnimation {
-                document.clear()
-              }
-            }
-            Button("Shake") {
-              withAnimation {
-                document.shakeDemo()
-              }
-            }
-            Button("Color") {
-              withAnimation {
-                document.colorDemo()
-              }
-            }
-          }
-          .buttonStyle(.bordered)
-          HStack {
-            Picker("Palette", selection: $document.selectedPalette) {
-              Text("rgb").tag(Palette.rgb)
-              Text("fixed").tag(Palette.fixed)
-            }
-            Button("Move-Back") {
-              withAnimation {
-                document.sendToBack()
-              }
-            }
-            Button("Save") {
-              document.save("chipItems.json")
-            }
-            Button("Restore") {
-              document.restore("chipItems.json")
-            }
-          }
-          .buttonStyle(.bordered)
-          Text("frame \(format(rect))")
+          SelectedItemView();
+          BottomButtonsView(rect: rect);
         }
         .padding(20)
         .onAppear {
@@ -113,6 +41,98 @@ struct ContentView: View {
     }
   }
 }
+
+struct BottomButtonsView: View {
+  var rect: CGRect;
+  
+  @Environment(Document.self) var document
+  var body: some View {
+    @Bindable var document = document
+    HStack {
+      Button("Add") {
+        withAnimation {
+          document.addItem(rect: rect)
+        }
+      }
+      Button("+8") {
+        withAnimation {
+          //document.clear();
+          document.addItems(rect: rect, count: 8)
+        }
+      }
+      Button("Clear") {
+        withAnimation {
+          document.clear()
+        }
+      }
+      Button("Shake") {
+        withAnimation {
+          document.shakeDemo()
+        }
+      }
+      Button("Color") {
+        withAnimation {
+          document.colorDemo()
+        }
+      }
+    }
+    .buttonStyle(.bordered)
+    HStack {
+      Picker("Palette", selection: $document.selectedPalette) {
+        Text("rgb").tag(Palette.rgb)
+        Text("fixed").tag(Palette.fixed)
+      }
+      Button("Move-Back") {
+        withAnimation {
+          document.sendToBack()
+        }
+      }
+      Button("Save") {
+        document.save("chipItems.json")
+      }
+      Button("Restore") {
+        document.restore("chipItems.json")
+      }
+    }
+    .buttonStyle(.bordered)
+    Text("frame \(format(rect))")
+
+  }
+}
+
+struct SelectedItemView: View {
+  @Environment(Document.self) var document
+  var body: some View {
+    @Bindable var document = document
+    if let item = document.selectedItem {
+      Text("x \(item.x) y \(item.y) color \(item.colorName)")
+      HStack {
+        ColorPicker("Color", selection: $document.itemColor)
+        Button("Rotate") {
+          document.update(id: document.selectedId, rotationBy: 45.0)
+        }
+        Button("+Size") {
+          document.update(id: document.selectedId, sizeBy: 1.1)
+        }
+        Button("-Size") {
+          document.update(id: document.selectedId, sizeBy: 0.9)
+        }
+      }
+      .buttonStyle(.bordered)
+      HStack {
+        Text("AssetName:")
+        Picker("AssetName", selection: $document.itemAssetName) {
+          Text("").tag("")
+          Text("cat").tag("cat")
+          Text("lama").tag("lama")
+        }
+      }
+      .padding(5)
+      .background(Color.gray)
+    }
+  }
+}
+
 
 #Preview {
   ContentView()
