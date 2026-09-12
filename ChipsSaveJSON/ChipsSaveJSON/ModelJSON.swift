3778ac1f85cfebf344108cbd717f2fd673c88e94
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2026-09-12 12:27:24 -0400

    Update ModelJSON.swift

--- ChipsSaveJSON/ChipsSaveJSON/ModelJSON.swift
+++ ChipsSaveJSON/ChipsSaveJSON/ModelJSON.swift
@@ -8,13 +8,15 @@ extension Model {
 
   func saveAsJSON(fileName: String) {
     do {
-      let directory =
-        try FileManager.default.url(
-          for: .documentDirectory,
-          in: .userDomainMask,
-          appropriateFor: nil,
-          create: true)
-      let fileUrl = directory.appendingPathComponent(fileName)
+//      let directory =
+//        try FileManager.default.url(
+//          for: .documentDirectory,
+//          in: .userDomainMask,
+//          appropriateFor: nil,
+//          create: true)
+      let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+
+      let fileUrl = directory!.appendingPathComponent(fileName)
       print("saveAsJSON filePath \(fileUrl as Any)")
 
       let encoder = JSONEncoder()
